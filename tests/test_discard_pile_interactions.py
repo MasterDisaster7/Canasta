3cc58fa9eef912a76057f4422db898a5b3a970bf
MasterDisaster7 <45339840+MasterDisaster7@users.noreply.github.com>
2025-07-06 20:58:19 +0200

    Fix test discovery in pre-commit

--- tests/test_discard_pile_interactions.py
+++ tests/test_discard_pile_interactions.py
@@ -2,12 +2,13 @@ import unittest
 from src.module.card import Card
 from src.module.game import Game
 
+
 class TestCanastaGameState(unittest.TestCase):
     def setUp(self):
         """Set up the game state for testing."""
 
         # Setup game with the player and discard pile
-        self.game = Game(player_names=['Player1', 'Player2', 'Player3', 'Player4'])
+        self.game = Game(player_names=["Player1", "Player2", "Player3", "Player4"])
         self.player = self.game.current_player()  # Get the first player
         self.discard_pile = self.game.discard_pile
 
@@ -34,15 +35,19 @@ class TestCanastaGameState(unittest.TestCase):
 
     def test_player_can_grab_discard_pile_with_doubles(self):
         # Add some cards to the discard pile for initial setup
-        self.set_discard_pile([
-            Card('4', 'Hearts'),
-            Card('5', 'Hearts'),
-            Card('6', 'Hearts'),
-            Card('7', 'Hearts')   # This will be the top card
-        ])
+        self.set_discard_pile(
+            [
+                Card("4", "Hearts"),
+                Card("5", "Hearts"),
+                Card("6", "Hearts"),
+                Card("7", "Hearts"),  # This will be the top card
+            ]
+        )
 
         # Set up the player's hand for this test
-        self.set_player_hand([Card('4', 'Hearts'), Card('7', 'Clubs'), Card('7', 'Clubs')])
+        self.set_player_hand(
+            [Card("4", "Hearts"), Card("7", "Clubs"), Card("7", "Clubs")]
+        )
 
         # Check if player can legally pick up the discard pile
         can_grab = self.player.can_pick_up_discard(self.discard_pile)
@@ -52,31 +57,43 @@ class TestCanastaGameState(unittest.TestCase):
         self.player.pick_up_discard_pile(self.discard_pile)
 
         # Verify the discard pile is now empty
-        self.assertEqual(len(self.game.discard_pile.cards), 0, "Discard pile should be empty after grabbing")
+        self.assertEqual(
+            len(self.game.discard_pile.cards),
+            0,
+            "Discard pile should be empty after grabbing",
+        )
 
         # Verify the player’s hand includes old hand + discard pile cards
         expected_hand = [
-            Card('4', 'Hearts'),
-            Card('7', 'Clubs'),
-            Card('7', 'Clubs'),
-            Card('4', 'Hearts'),
-            Card('5', 'Hearts'),
-            Card('6', 'Hearts'),
-            Card('7', 'Hearts'),
+            Card("4", "Hearts"),
+            Card("7", "Clubs"),
+            Card("7", "Clubs"),
+            Card("4", "Hearts"),
+            Card("5", "Hearts"),
+            Card("6", "Hearts"),
+            Card("7", "Hearts"),
         ]
-        self.assertCountEqual(self.player.hand.get_all_cards(), expected_hand, "Player's hand should include all discard pile cards")
-    
+        self.assertCountEqual(
+            self.player.hand.get_all_cards(),
+            expected_hand,
+            "Player's hand should include all discard pile cards",
+        )
+
     def test_player_can_grab_discard_pile_with_wildcard(self):
         # Add some cards to the discard pile for initial setup
-        self.set_discard_pile([
-            Card('4', 'Hearts'),
-            Card('5', 'Hearts'),
-            Card('6', 'Hearts'),
-            Card('7', 'Hearts')   # This will be the top card
-        ])
+        self.set_discard_pile(
+            [
+                Card("4", "Hearts"),
+                Card("5", "Hearts"),
+                Card("6", "Hearts"),
+                Card("7", "Hearts"),  # This will be the top card
+            ]
+        )
 
         # Set up the player's hand for this test
-        self.set_player_hand([Card('4', 'Hearts'), Card('7', 'Clubs'), Card('2', 'Clubs')])
+        self.set_player_hand(
+            [Card("4", "Hearts"), Card("7", "Clubs"), Card("2", "Clubs")]
+        )
 
         # Check if player can legally pick up the discard pile
         can_grab = self.player.can_pick_up_discard(self.discard_pile)
@@ -84,16 +101,21 @@ class TestCanastaGameState(unittest.TestCase):
 
     def test_player_can_grab_frozen_discard_pile_with_doubles(self):
         # Add some cards to the discard pile for initial setup
-        self.set_discard_pile([
-            Card('4', 'Hearts'),
-            Card('5', 'Hearts'),
-            Card('6', 'Hearts'),
-            Card('2', 'Hearts'),  # This card will be used to freeze the discard pile
-            Card('7', 'Hearts')   # This will be the top card
-        ])
+        self.set_discard_pile(
+            [
+                Card("4", "Hearts"),
+                Card("5", "Hearts"),
+                Card("6", "Hearts"),
+                # This card will be used to freeze the discard pile
+                Card("2", "Hearts"),
+                Card("7", "Hearts"),  # This will be the top card
+            ]
+        )
 
         # Set up the player's hand for this test
-        self.set_player_hand([Card('4', 'Hearts'), Card('7', 'Clubs'), Card('7', 'Clubs')])
+        self.set_player_hand(
+            [Card("4", "Hearts"), Card("7", "Clubs"), Card("7", "Clubs")]
+        )
 
         # Check if player can legally pick up the discard pile
         can_grab = self.player.can_pick_up_discard(self.discard_pile)
@@ -103,37 +125,53 @@ class TestCanastaGameState(unittest.TestCase):
         self.player.pick_up_discard_pile(self.discard_pile)
 
         # Verify the discard pile is now empty
-        self.assertEqual(len(self.game.discard_pile.cards), 0, "Discard pile should be empty after grabbing")
+        self.assertEqual(
+            len(self.game.discard_pile.cards),
+            0,
+            "Discard pile should be empty after grabbing",
+        )
 
         # Verify the player’s hand includes old hand + discard pile cards
         expected_hand = [
-            Card('4', 'Hearts'),
-            Card('7', 'Clubs'),
-            Card('7', 'Clubs'),
-            Card('4', 'Hearts'),
-            Card('5', 'Hearts'),
-            Card('6', 'Hearts'),
-            Card('2', 'Hearts'),
-            Card('7', 'Hearts'),
+            Card("4", "Hearts"),
+            Card("7", "Clubs"),
+            Card("7", "Clubs"),
+            Card("4", "Hearts"),
+            Card("5", "Hearts"),
+            Card("6", "Hearts"),
+            Card("2", "Hearts"),
+            Card("7", "Hearts"),
         ]
-        self.assertCountEqual(self.player.hand.get_all_cards(), expected_hand, "Player's hand should include all discard pile cards")
-    
+        self.assertCountEqual(
+            self.player.hand.get_all_cards(),
+            expected_hand,
+            "Player's hand should include all discard pile cards",
+        )
+
     def test_player_cant_grab_frozen_discard_pile_with_wildcard(self):
         # Add some cards to the discard pile for initial setup
-        self.set_discard_pile([
-            Card('4', 'Hearts'),
-            Card('5', 'Hearts'),
-            Card('6', 'Hearts'),
-            Card('2', 'Hearts'),  # This card will be used to freeze the discard pile
-            Card('7', 'Hearts')   # This will be the top card
-        ])
+        self.set_discard_pile(
+            [
+                Card("4", "Hearts"),
+                Card("5", "Hearts"),
+                Card("6", "Hearts"),
+                # This card will be used to freeze the discard pile
+                Card("2", "Hearts"),
+                Card("7", "Hearts"),  # This will be the top card
+            ]
+        )
 
         # Set up the player's hand for this test
-        self.set_player_hand([Card('4', 'Hearts'), Card('7', 'Clubs'), Card('2', 'Clubs')])
+        self.set_player_hand(
+            [Card("4", "Hearts"), Card("7", "Clubs"), Card("2", "Clubs")]
+        )
 
         # Check if player can legally pick up the discard pile
         can_grab = self.player.can_pick_up_discard(self.discard_pile)
-        self.assertFalse(can_grab, "Player should not be allowed to grab the discard pile")
+        self.assertFalse(
+            can_grab, "Player should not be allowed to grab the discard pile"
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
